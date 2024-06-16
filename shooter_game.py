2605d4f4e226e046baf1b3cbfbcc2e738dbd089e
MrWir4uk <152081630+MrWir4uk@users.noreply.github.com>
2024-06-16 17:33:26 +0300

    ffwfwf

--- shooter_game.py
+++ shooter_game.py
@@ -1,6 +1,7 @@
 from random import choice, randint
 from typing import Any
 from pygame import *
+import sys
 
 init()
 font.init()
@@ -56,18 +57,21 @@ class Player(Sprite):
         new_bullet = Bullet(bullet_img, 10, 20, self.rect.left, self.rect.top)
         new_bullet = Bullet(bullet_img, 10, 20, self.rect.right-10, self.rect.top)
         bullet_sound.play()
- 
     
+
 
     def update(self):
         key_pressed = key.get_pressed()
         old_pos = self.rect.x, self.rect.y
         if key_pressed[K_w] and self.rect.y > 200:
             self.rect.y -= self.speed
-            if self.bg_speed < self.max_speed:
-                self.bg_speed += 0.1
+            self.bg_speed += 0.1
         if key_pressed[K_s] and self.rect.bottom < HEIGHT:
             self.rect.y += self.speed
+            if self.bg_speed > 2:
+                self.bg_speed -= 0.2
+
+
         if key_pressed[K_a] and self.rect.x > 0:
             self.rect.x -= self.speed
         if key_pressed[K_d] and self.rect.right < WIDTH:
@@ -121,6 +125,9 @@ spawn_interval = randint(500, 3000)
 run = True
 finish = False
 
+start_screen = True
+start_text = font2.render(f"PRESS ANT KEY TO START", True, (255,255,255))
+restart_text = font2.render(f"PRESS R KEY TO RESTART", True, (255,255,255))
 
 
 while run:
@@ -128,50 +135,70 @@ while run:
         if e.type == QUIT:
             run = False
         if e.type == KEYDOWN:
+            if start_screen:
+                start_screen = False
             if e.key == K_ESCAPE:
                 run = False
-            if e.key == K_SPACE:
+                sys.exit()
+            if e.key == K_SPACE and not start_screen and not finish:
                 player.shoot()
+            if finish and e.key == K_r:
+                finish = False                  
+                for s in all_sprites:
+                    s.kill()
+                    player = Player(player_img, 70, 50, 300, 300)
+                    score_text = font2.render(f"Score:{player.score}", True, (255,255,255))
 
 
     window.blit(bg,(0,bg_y1))
     window.blit(bg,(0,bg_y2))
-    bg_y1+=player.bg_speed
-    bg_y2+=player.bg_speed
-    
-    if bg_y1 > HEIGHT:
-        bg_y1 = -HEIGHT
-    if bg_y2 > HEIGHT:
-        bg_y2 = -HEIGHT
 
-    if player.hp <= 0:
-        finish = True
-    
-    now = time.get_ticks() #поточний час
-    if now - enemy_spawn_time > spawn_interval: #Якщо від появи останнього ворога пройшло <1 секундни
-        enemy1 = Enemy(enemy_img, 80,60)#створюємо нового ворога
-        enemy_spawn_time = time.get_ticks() #оновлюємо час появи ворога
-        spawn_interval = randint(1000, 5000) 
-
-    bullet_collide = sprite.groupcollide(bullets,enemys,True,True,sprite.collide_mask)
-    for bullet in bullet_collide:
-        player.score += 10
-        score_text = font2.render(f"Score:{player.score}", True, (255,255,255))
+    if start_screen:
+        window.blit(start_text,(500,300))
+    else:
+        bg_y1+=player.bg_speed
+        bg_y2+=player.bg_speed
         
+        if bg_y1 > HEIGHT:
+            bg_y1 = -HEIGHT
+        if bg_y2 > HEIGHT:
+            bg_y2 = -HEIGHT
 
-    collide_list = sprite.spritecollide(player, enemys, True, sprite.collide_mask)
-    if len(collide_list) > 0:
-        finish = True
+        if player.hp <= 0:
+            finish = True
+        
+        now = time.get_ticks() #поточний час
+        if now - enemy_spawn_time > spawn_interval: #Якщо від появи останнього ворога пройшло <1 секундни
+            enemy1 = Enemy(enemy_img, 80,60)#створюємо нового ворога
+            enemy_spawn_time = time.get_ticks() #оновлюємо час появи ворога
+            spawn_interval = randint(500, 1500) 
+
+        bullet_collide = sprite.groupcollide(bullets,enemys,True,True,sprite.collide_mask)
+        for bullet in bullet_collide:
+            player.score += 10
+            score_text = font2.render(f"Score:{player.score}", True, (255,255,255))
+            
+
+        collide_list = sprite.spritecollide(player, enemys, True, sprite.collide_mask)
+        if len(collide_list) > 0:
+            finish = True
 
 
 
-    all_sprites.draw(window)
-    window.blit(score_text,(30,30))
-    if not finish:
-        all_sprites.update()
-    if finish:
-        window.blit(game_over_text, (300,500))
-#game_over_text,WIDTH/2 - game_over_text.get_width()/2, HEIGHT/2 - game_over_text.get_height()/2
+        all_sprites.draw(window)
+        window.blit(score_text,(30,30))
+        if not finish:
+            all_sprites.update()
+        if finish:
+            window.blit(game_over_text, (300,500))
+            window.blit(restart_text, (500,600))
+    #game_over_text,WIDTH/2 - game_over_text.get_width()/2, HEIGHT/2 - game_over_text.get_height()/2
 
     display.update()
     clock.tick(FPS)
+
+
+
+
+
+    
